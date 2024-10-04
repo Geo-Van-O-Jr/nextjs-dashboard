9c023cacda8c90dae3c3149b89c74a1a5604ebac
geovano <geovanejr23@outlook.com>
2024-10-04 20:24:57 -0300

    V12

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
-
-const nextConfig = {};
-
-export default nextConfig;
+ 
+const nextConfig = {
+    experimental: {
+      ppr: 'incremental',
+    },
+  };
+   
+  export default nextConfig;
