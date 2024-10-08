92896bd5641e80b0bbf73c5caef3c97fe5dacf4c
geovano <geovanejr23@outlook.com>
2024-10-08 13:04:50 -0300

    Adding Server Action

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,26 @@
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+
+export default async function Page({ params }: { params: { id: string } }) {
+    const id = params.id;
+    const [invoice, customers] = await Promise.all([
+        fetchInvoiceById(id), // Use 'id' instead of 'Id'
+        fetchCustomers(),
+    ]);
+    return (
+        <main>
+            <Breadcrumbs
+                breadcrumbs={[
+                    { label: 'Invoices', href: '/dashboard/invoices' },
+                    {
+                        label: 'Edit Invoice',
+                        href: `/dashboard/invoices/${id}/edit`, // Use 'id' instead of 'Id'
+                        active: true,
+                    },
+                ]}
+            />
+            <Form invoice={invoice} customers={customers} />
+        </main>
+    );
+}
