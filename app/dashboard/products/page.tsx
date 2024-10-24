ab0c476f96583269eb641a4d71e09d2265c28778
geovano <geovanejr23@outlook.com>
2024-10-24 03:56:42 -0300

    commit

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -58,7 +58,7 @@ export default async function Page({
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        <Pagination totalPages={totalPages} />
+        <Pagination totalPages={Number(totalPages) || 1} />
       </div>
     </div>
   );
